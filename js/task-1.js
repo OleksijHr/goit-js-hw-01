79500899690423181eace26e2c3364936332c0f3
OleksijHr <hrymovskij@gmail.com>
2024-01-25 22:25:32 +0200

    reight structure

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,3 @@
-'use strict';
-
 function makeTransaction(quantity, preicePerDroid) {
     let totalPrice = quantity * preicePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,3 @@
-'use strict';
-
 function getShippingMassage(country, price, deliveryFee) {
     let totalPrice = price + deliveryFee;
     return `Shipping to ${country} will cost ${totalPrice} credits`;
